f5529aeaefeaf9078d43f650c78709793e80085e
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-10-19 09:31:10 +0300

    add config-repo with git (for all)

--- user-service/src/main/java/com/users/controllers/PingController.java
+++ user-service/src/main/java/com/users/controllers/PingController.java
@@ -1,5 +1,6 @@
 package com.users.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,11 @@ import java.time.LocalDateTime;
 public class PingController {
     private String currentDate = LocalDateTime.now().toLocalDate().toString();
 
+    @Value("${tokenSecret}")
+    String port;
 
     @GetMapping()
     public String ping() {
-        return " Users-Service working " + currentDate;
+        return " Users-Service working " + currentDate + " on port " + port;
     }
 }
