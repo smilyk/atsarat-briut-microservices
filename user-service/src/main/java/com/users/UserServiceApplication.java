4da6f4fb4db8dce8a1a177ccb318f5e90109e95e
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-10-20 20:00:00 +0300

    config from git

--- user-service/src/main/java/com/users/UserServiceApplication.java
+++ user-service/src/main/java/com/users/UserServiceApplication.java
@@ -3,13 +3,14 @@ package com.users;
 import com.users.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
-
+@RefreshScope
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
