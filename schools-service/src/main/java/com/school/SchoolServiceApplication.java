4f794ec1a91a8bef0b22b9c09ced425cbd7fd685
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-11-01 12:00:57 +0200

    add hystrix-monitor to school-service and tsofim-service

--- schools-service/src/main/java/com/school/SchoolServiceApplication.java
+++ schools-service/src/main/java/com/school/SchoolServiceApplication.java
@@ -2,10 +2,17 @@ package com.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
+
 public class SchoolServiceApplication {
 
 	public static void main(String[] args) {
