9dba1fc6ac44efb5ca02068ac16253ffbd817ab8
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-11-01 10:23:00 +0200

    code review

--- gymnast-sarvice/src/main/java/com/gymnast/GymnastServerApplication.java
+++ gymnast-sarvice/src/main/java/com/gymnast/GymnastServerApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
 @EnableFeignClients
+@EnableHystrix
 public class GymnastServerApplication {
 
 
