580b277faec48fd90f18f837306e6a49b1657ad6
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-11-25 17:46:38 +0200

    add CI CD

--- user-service/src/main/java/com/users/enums/RequestOperationName.java
+++ user-service/src/main/java/com/users/enums/RequestOperationName.java
@@ -1,7 +1,5 @@
 package com.users.enums;
 
-import javafx.beans.property.adapter.JavaBeanBooleanPropertyBuilder;
-
 public enum RequestOperationName {
     VERIFY_EMAIL,DELETE,PASSWORD_RESET,
     ;
